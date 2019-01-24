7f0be37e150912a669d1db72b90edb18999aa6c9
Riza Fahmi <rizafahmi@gmail.com>
2019-01-24 10:20:50 +0700

    Tanam bug

--- calc.js
+++ calc.js
@@ -5,7 +5,7 @@ const add = (a, b) => {
     return parseFloat(a) + parseFloat(b);
   return parseInt(a) + parseInt(b);
 };
-const sub = (a, b) => a - b;
+const sub = (a, b) => a + b;
 const mul = (a, b) => a * b;
 const div = (a, b) => a / b;
 
